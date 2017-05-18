1cf4a0aa8be9876bb9405829a806c2deebacc635
LocBui <bploc.91@gmail.com>
2017-05-18 16:25:28 +0700

    create new config reference SDK

--- MusicManager/source/class/musicmanager/Application.js
+++ MusicManager/source/class/musicmanager/Application.js
@@ -52,48 +52,85 @@ qx.Class.define("musicmanager.Application",
         Below is your actual application code...
       -------------------------------------------------------------------------
       */
+	  // Document is the application root
+      var doc = this.getRoot();
+	  
+	  var groupBoxMusicSong = new qx.ui.groupbox.GroupBox("SongManager");
+      groupBoxMusicSong.setWidth(320);
+      doc.add(groupBoxMusicSong, {left: 10, top: 10});
+      var gridMusicSong = new qx.ui.layout.Grid();
+      gridMusicSong.setSpacing(5);
+      gridMusicSong.setColumnAlign(0, "left", "middle")
+      groupBoxMusicSong.setLayout(gridMusicSong);
+	  
+	  var labelName = new qx.ui.basic.Label("Name: ");
+	  groupBoxMusicSong.add(labelName, {row:0, column:0});
+	  
+	  var textFieldName= new qx.ui.form.TextField("");
+	  groupBoxMusicSong.add(textFieldName,{row:0, column:1});
+	  
+	  var labelAuthor = new qx.ui.basic.Label("Author: ");
+	  groupBoxMusicSong.add(labelAuthor,{row:0, column:2});
+	  
+	  var textFieldAuthor= new qx.ui.form.TextField("");
+	  groupBoxMusicSong.add(textFieldAuthor,{row:, column:3});
+	  
+	  var labelSearch = new qx.ui.basic.Label("Search: ");
+	  groupBoxMusicSong.add(labelSearch,{row:0, column:4});
+	  
+	  var textFieldSearch= new qx.ui.form.TextField("");
+	  groupBoxMusicSong.add(textFieldSearch,{row:0, column:5});
+	  
+	  var buttonSearch = new qx.ui.form.Button("Search");
+	  groupBoxMusicSong.add(buttonSearch,{row:2, column:2});
+	  
+	  var buttonAdd = new qx.ui.form.Button("Add    ", "musicmanager/test.png");
+	  groupBoxMusicSong.add(buttonAdd,{row:3, column:0});
+	  
+	  var buttonDelete = new qx.ui.form.Button("Delete ", "musicmanager/test.png");
+	  groupBoxMusicSong.add(buttonDelete,{row:3, column:1});
+	  
+	  var buttonSignIn = new qx.ui.form.Button("Sign in", "musicmanager/test.png");
+	  groupBoxMusicSong.add(buttonSignIn,{row:3, column:2});
+	  
+	  var tableSongModel = new qx.ui.table.model.Simple();
+	  tableSongModel.setColumns(["Name", "Author"]);
+	  tableSongModel.setData([["My love", "West Life"],["One Love","A1"],["Yesterday Once More", "Carpenter Brothers"]]);
+	  var tableSong = new qx.ui.table.Table(tableSongModel);
+	  groupBoxMusicSong.add(tableSong,{row:4, column:0,colSpan: 3});
+	  
 
       // Create a button
-	  var label1 = new qx.ui.basic.Label("Name: ");
-	  var label2 = new qx.ui.basic.Label("Author: ");
-	  var txtTextField1= new qx.ui.form.TextField("");
-	  var txtTextField2= new qx.ui.form.TextField("");
-      var button1 = new qx.ui.form.Button("Add", "musicmanager/test.png");
-	  var button2 = new qx.ui.form.Button("Delete", "musicmanager/test.png");
 	  
-	  var tableModel = new qx.ui.table.model.Simple();
-	  tableModel.setColumns(["Name", "Author"]);
-	  tableModel.setData([["My love", "West Life"],["One Love","A1"],["Yesterday Once More", "Carpenter Brothers"]]);
 	  
-	  var table = new qx.ui.table.Table(tableModel);
-	  this.getRoot().add(table,{left:100, top:140});
-	  txtTextField1.setWidth(150);
-	  txtTextField2.setWidth(150);
-	  table.setWidth(300);
-	  table.setHeight(200);
-
-
-
-		
-	
-      // Document is the application root
-      var doc = this.getRoot();
+	  
+	  
+	  
+	  
+      
+	  
+	  
+	  
+	  //add the table
+	  
+	  
+	  //tableSong.setWidth(300);
+	  //tableSong.setHeight(200);
+	  textFieldName.setRequired(true);
+	  
 
       // Add button to document at fixed coordinates
-	  this.getRoot().add(label1, {left:100, top:10});
-	  this.getRoot().add(label2, {left:100, top:40});
-	  doc.add(txtTextField1,{left: 150, top: 10});
-	  doc.add(txtTextField2,{left: 150, top: 40});
-	  doc.add(button1, {left: 120, top: 80});
-	  doc.add(button2, {left: 210, top: 80});
+	  
+	  
 
       // Add an event listener
-      button1.addListener("execute", function(e) {
+      buttonAdd.addListener("execute", function(e) {
         alert("Hello World!");
       });
-	  button2.addListener("execute",function(e){
+	  buttonDelete.addListener("execute",function(e){
 		alert("OK")
 	  });
+	  
     }
   }
 });
