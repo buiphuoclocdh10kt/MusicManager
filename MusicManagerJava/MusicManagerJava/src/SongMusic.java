4833670feebaeedf750fcce4f8952b040434ee95
LocBui <bploc.91@gmail.com>
2017-05-24 08:21:03 +0700

    update example Hibernate

---
+++ MusicManagerJava/MusicManagerJava/src/SongMusic.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class SongMusic {
+	int id;
+	String name;
+	String author;
+	Date date;
+	Boolean status;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+	
+	
+
+}
